c53bba8c74923d197acfa0f8fe7cbceb0689086c
L. E. <Lozovan.E@yandex.ru>
2024-02-19 16:54:44 +0300

    UI2 Arch sample project for WSh and HW.

---
+++ mtp_aqa_api_tests_architecture_workshop-n-homework/app/src/main/java/com/mtp/aqa/wrkshp/api/arch/App.java
@@ -0,0 +1,10 @@
+package com.mtp.aqa.wrkshp.api.arch;
+
+import static java.lang.System.out;
+
+public class App {
+    public static void main(String[] args) {
+        // Just an entry point.
+        out.println("Let's the testing begin!");
+    }
+}
